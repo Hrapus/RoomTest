a7c3d6ef2897325c4a3609c07df574100369d0b1
Hrapus <hrapus@gmail.com>
2022-10-12 19:46:36 -0400

    Initial

---
+++ app/src/main/java/com/example/roomtest/MainDb.kt
@@ -0,0 +1,24 @@
+package com.example.roomtest
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.DatabaseConfiguration
+import androidx.room.InvalidationTracker
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+
+@Database(entities = [Item::class], version = 1)
+abstract class MainDb: RoomDatabase() {
+    abstract fun getDao(): Dao
+
+    companion object{
+        fun getDb(context: Context): MainDb{
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MainDb::class.java,
+            "test.db"
+            ).build()
+        }
+    }
+}
